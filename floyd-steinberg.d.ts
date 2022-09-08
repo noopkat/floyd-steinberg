7512c19b70952f97f92545f6446cbed316a2d70b
Marek Lukáš <tajnymag@gmail.com>
2022-09-08 17:35:54 +0200

    Improved typings based on typescript recommendations

--- floyd-steinberg.d.ts
+++ floyd-steinberg.d.ts
@@ -1,4 +1,6 @@
-type ImageDataSubset = Partial<ImageData> & Pick<ImageData, 'width' | 'height' | 'data'>;
+declare module "floyd-steinberg" {
+    type ImageDataSubset = Partial<ImageData> & Pick<ImageData, 'width' | 'height' | 'data'>;
 
-declare function floyd_steinberg<T extends ImageDataSubset>(image: T): T;
-export default floyd_steinberg;
+    function floyd_steinberg<T extends ImageDataSubset>(image: T): T;
+    export = floyd_steinberg;
+}
